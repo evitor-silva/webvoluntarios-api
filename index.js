8ea511782e4401397024ac49d7b988ee7fb6e878
evitorsilva <ceja.vitorgabriel.e.silva@gmail.com>
2026-04-15 16:44:56 -0300

    update files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./src/routes');
 const cors = require('cors'); // Importe o pacote cors
 const app = express()
-const port = 3333;
+const port =  process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
